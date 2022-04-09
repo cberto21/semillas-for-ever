446035b643d92cdc35f4d42401875c6286c7f34e
cberto <bertolanicami@gmail.com>
2022-04-09 19:28:47 -0300

    update

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
@@ -1,37 +1,47 @@
-package ar.edu.unahur.obj2.semillas
+package ar.edu.unahur.obj2.ejemplo
 
-class Menta(var altura: Double, val anioSemilla: Int) {
-    fun espacio(): Double {
-        TODO("Te la debo, amigue...")
+abstract class Planta(val anioSemilla : Int, var altura: Double) { // SUPER CLASS con ABSTRAC para funciones abstractas
+    object Constantes{
+        val HORAS_TOLERADAS = 9
     }
-
-    fun daSemillas(): Boolean {
-        TODO("Colgue, che...")
-    }
-
-    fun esFuerte(): Boolean {
-        TODO("Estas seguro que lo queres implementar así?")
-    }
-
+    open fun horasDeSolTolera() = 7
+    fun esFuerte() = horasDeSolTolera() > Planta.Constantes.HORAS_TOLERADAS
+    open fun daSemilla() = esFuerte() || condAlternativa()
+    abstract fun espacio() : Double //DECLARAR SIEMPRE EL TIPO cuando son abstract
+    abstract fun condAlternativa() : Boolean
 }
 
-class Soja(var altura: Double, val anioSemilla: Int) {
-    fun espacio(): Double {
-        TODO("Lee un poco más el enunciado...")
-    }
-
-    fun daSemillas(): Boolean {
-        TODO("Dalee")
-    }
-
-    fun esFuerte(): Boolean {
-        TODO("Así me implementas mejor")
-    }
+open class Menta(anioSemilla: Int, altura: Double) : Planta(anioSemilla, altura){ // open HABILITA CAMBIOS EN SUBCLASS
+    override fun espacio() = altura + 1.0
+    override fun condAlternativa() = altura > 0.4
 }
 
-class Planta(var altura: Double, val anioSemilla: Int) {
-    fun espacio() {
-        TODO("Pasaron cosas")
+open class Soja(anioSemilla: Int, altura: Double) : Planta(anioSemilla,altura){
+    override fun horasDeSolTolera(): Int { // SIN RETURN CON "=" HAY QUE DECLARAR TIPO
+        return (if (altura > 0.5) {6}
+        else if (altura in 0.5..1.0){8}
+        else {12})
     }
+    override fun condAlternativa() = anioSemilla < 2007 && altura in 0.75..0.9
+    override fun espacio() = altura/2 // CON RETURN CON "=" NO HACE FALTA DECLARAR TIPO
+}
 
+abstract class Quinoa(anioSemilla: Int, altura: Double): Planta(anioSemilla,altura){
+    abstract var espacio: Double
+    override fun horasDeSolTolera(): Int {
+        return (if(espacio() < 0.3){
+            10
+        }else{
+            super.horasDeSolTolera()
+        })
+    }
+    override fun espacio() = espacio
+    override fun condAlternativa() = anioSemilla in 2001..2008
+}
+
+class SojaTrangenica(anioSemilla: Int,altura: Double) : Soja(anioSemilla,altura){
+    override fun daSemilla() = false
+}
+class Peperina(anioSemilla: Int, altura: Double) : Menta(anioSemilla,altura){
+    override fun espacio() = super.espacio() * 2
 }
--- src/test/kotlin/ar/edu/unahur/obj2/semillas/SemillasTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillas/SemillasTest.kt
@@ -4,7 +4,7 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
-
+/*
 class SemillasTest : DescribeSpec ({
     // hay una clase Planta que tiene por atributos
     // anioSemilla, altura
@@ -45,3 +45,6 @@ class SemillasTest : DescribeSpec ({
         }
     }
 })
+
+
+ */
