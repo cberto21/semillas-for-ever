7f6dca72f25aac089978284eb7719a90efbae3a7
cberto21 <camila.bertolani@estudiantes.unahur.edu.ar>
2022-04-09 19:36:58 -0300

    Update

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
@@ -1,29 +1,29 @@
 package ar.edu.unahur.obj2.ejemplo
 
-abstract class Planta(val anioSemilla : Int, var altura: Double) { // SUPER CLASS con ABSTRAC para funciones abstractas
-    object Constantes{
+abstract class Planta(val anioSemilla : Int, var altura: Double) {
+    object Constantes {
         val HORAS_TOLERADAS = 9
     }
     open fun horasDeSolTolera() = 7
     fun esFuerte() = horasDeSolTolera() > Planta.Constantes.HORAS_TOLERADAS
     open fun daSemilla() = esFuerte() || condAlternativa()
-    abstract fun espacio() : Double //DECLARAR SIEMPRE EL TIPO cuando son abstract
+    abstract fun espacio() : Double
     abstract fun condAlternativa() : Boolean
 }
 
-open class Menta(anioSemilla: Int, altura: Double) : Planta(anioSemilla, altura){ // open HABILITA CAMBIOS EN SUBCLASS
+open class Menta(anioSemilla: Int, altura: Double) : Planta(anioSemilla, altura){
     override fun espacio() = altura + 1.0
     override fun condAlternativa() = altura > 0.4
 }
 
 open class Soja(anioSemilla: Int, altura: Double) : Planta(anioSemilla,altura){
-    override fun horasDeSolTolera(): Int { // SIN RETURN CON "=" HAY QUE DECLARAR TIPO
+    override fun horasDeSolTolera(): Int {
         return (if (altura > 0.5) {6}
         else if (altura in 0.5..1.0){8}
         else {12})
     }
     override fun condAlternativa() = anioSemilla < 2007 && altura in 0.75..0.9
-    override fun espacio() = altura/2 // CON RETURN CON "=" NO HACE FALTA DECLARAR TIPO
+    override fun espacio() = altura / 2
 }
 
 abstract class Quinoa(anioSemilla: Int, altura: Double): Planta(anioSemilla,altura){
@@ -35,8 +35,9 @@ abstract class Quinoa(anioSemilla: Int, altura: Double): Planta(anioSemilla,altu
             super.horasDeSolTolera()
         })
     }
-    override fun espacio() = espacio
     override fun condAlternativa() = anioSemilla in 2001..2008
+    override fun espacio() = espacio
+
 }
 
 class SojaTrangenica(anioSemilla: Int,altura: Double) : Soja(anioSemilla,altura){
