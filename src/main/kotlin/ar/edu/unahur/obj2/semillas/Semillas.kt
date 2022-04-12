2bde0b15c260c0139f473375f705afcc49dab260
Daniela Capponi <82007389+danielaCapponi@users.noreply.github.com>
2022-04-11 23:46:11 -0300

    Agrego tests y corrijo algunas características de las plantas

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
@@ -5,7 +5,7 @@ abstract class Planta(val anioSemilla : Int, var altura: Double) {
         val HORAS_TOLERADAS = 9
     }
     open fun horasDeSolTolera() = 7
-    fun esFuerte() = horasDeSolTolera() > Planta.Constantes.HORAS_TOLERADAS
+    private fun esFuerte() = horasDeSolTolera() > Constantes.HORAS_TOLERADAS
     open fun daSemilla() = esFuerte() || condAlternativa()
     abstract fun espacio() : Double
     abstract fun condAlternativa() : Boolean
@@ -18,16 +18,15 @@ open class Menta(anioSemilla: Int, altura: Double) : Planta(anioSemilla, altura)
 
 open class Soja(anioSemilla: Int, altura: Double) : Planta(anioSemilla,altura){
     override fun horasDeSolTolera(): Int {
-        return (if (altura > 0.5) {6}
+        return (if (altura < 0.5) {6}
         else if (altura in 0.5..1.0){8}
         else {12})
     }
-    override fun condAlternativa() = anioSemilla < 2007 && altura in 0.75..0.9
+    override fun condAlternativa() = anioSemilla > 2007 && altura in 0.75..0.9
     override fun espacio() = altura / 2
 }
 
-abstract class Quinoa(anioSemilla: Int, altura: Double): Planta(anioSemilla,altura){
-    abstract var espacio: Double
+open class Quinoa(anioSemilla: Int, altura: Double, var espacio: Double): Planta(anioSemilla,altura){
     override fun horasDeSolTolera(): Int {
         return (if(espacio() < 0.3){
             10
--- src/test/kotlin/ar/edu/unahur/obj2/semillas/SemillasTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillas/SemillasTest.kt
@@ -1,50 +1,76 @@
 package ar.edu.unahur.obj2.semillas
 
+import ar.edu.unahur.obj2.ejemplo.*
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
-/*
-class SemillasTest : DescribeSpec ({
-    // hay una clase Planta que tiene por atributos
-    // anioSemilla, altura
-    describe("Creación de las plantas") {
-        val menta = Menta(1.0, 2021)
-        val mentita = Menta(0.3, 2021)
-        val soja = Soja(0.6, 2009)
 
-        it("probamos los atributos altura  y anioSemilla") {
-            menta.altura.shouldBe(1.0)
-            menta.anioSemilla.shouldBe(2021)
-        }
 
-        it("verificar si da semillas") {
-            menta.daSemillas().shouldBeTrue()
-            mentita.daSemillas().shouldBeFalse()
-            soja.daSemillas().shouldBeFalse()
-        }
+class SemillaTest: DescribeSpec({
+    val menta = Menta(2021, 1.0)
+    val mentita = Menta( 2021,0.3)
+    val sojaAlta = Soja(2008,0.8)
+    val sojaBaja = Soja(2009,0.6)
+    val sojaTransgenica = SojaTrangenica(2005,0.8)
+    val quinoaReducida = Quinoa(2010, 0.9, 0.2)
+    val quinoaExtendida2010 = Quinoa(2010, 0.9, 0.9)
+    val quinoaExtendida2006 = Quinoa(2006, 0.9, 0.9)
+    val peperina = Peperina(2021,0.3)
 
-        it("es fuerte") {
-            menta.esFuerte().shouldBeFalse()
-            soja.esFuerte().shouldBeFalse()
-        }
-
-        it("espacio") {
-            menta.espacio().shouldBe(2.0)
-            mentita.espacio().shouldBe(1.3)
-            soja.espacio().shouldBe(0.3)
-        }
-
-        it("verifico la suma de varias") {
-            val superficie = mutableListOf(
-                soja.espacio(),
-                menta.espacio(),
-                mentita.espacio()
-            ).sum()
-            Math.ceil(superficie).shouldBe(4)
-        }
+    // Testear Menta
+    describe("Una menta de 1mt debería dar semillas"){
+        menta.daSemilla().shouldBeTrue()
     }
+    describe("Una menta de 1mt debería ocupar 2mt2"){
+        menta.espacio().shouldBe(2.0)
+    }
+    describe("Una menta de 0.3mt no debería dar semillas"){
+        mentita.daSemilla().shouldBeFalse()
+    }
+    describe("Una menta de 0.3mt debería ocupar 1.3m2"){
+        mentita.espacio().shouldBe(1.3)
+    }
+    describe("Una soja de 0.6 mt debería tener una tolerancia al sol de 8hs"){
+        sojaBaja.horasDeSolTolera().shouldBe(8)
+    }
+    describe("Una soja de 0.6 mt y de semilla 2009 no debería dar semillas"){
+        sojaBaja.daSemilla().shouldBeFalse()
+    }
+    describe("Una soja de 0.6 mt debería ocupar 0.3mt2"){
+        sojaBaja.espacio().shouldBe(0.3)
+    }
+    describe("Una soja de 0.8 mt debería tener una tolerancia al sol de 8hs"){
+        sojaAlta.horasDeSolTolera().shouldBe(8)
+    }
+    describe("Una soja de 0.8 mt y de semilla 2008 debería dar semillas"){
+        sojaAlta.daSemilla().shouldBeTrue()
+    }
+    describe("Una soja de 0.8 mt debería ocupar 0.4mt2"){
+        sojaAlta.espacio().shouldBe(0.4)
+    }
+    describe("Una quinoa que ocupa 0.2mt2 y con origen 2010 debería dar semillas"){
+        quinoaReducida.daSemilla().shouldBeTrue()
+    }
+    describe("Una quinoa que ocupa 0.9mt2 y con origen 2010 no debería dar semillas"){
+        quinoaExtendida2010.daSemilla().shouldBeFalse()
+    }
+    describe("Una quinoa que ocupa 0.9mt2 y con origen 2006 debería dar semillas"){
+        quinoaExtendida2006.daSemilla().shouldBeTrue()
+    }
+    describe("Una soja transgénica nunca da semillas, aunque cumpla la condición general"){
+        sojaTransgenica.daSemilla().shouldBeFalse()
+    }
+    describe("Una peperina debe ocupar el doble de espacio que la menta de mismas características"){
+peperina.espacio().shouldBe(mentita.espacio() * 2)
+    }
+    describe("Una parcela de ancho: 20m y largo: 1m tiene una superficie de 20mts2"){
+
+    }
+    describe("Una parcela de ancho: 20m y largo: 1m tolera hasta 4 plantas"){}
+    describe("Una parcela de ancho: 1m y largo: 9m tolera hasta 12 plantas"){}
+    describe("Una parcela que recibe 10hs de sol tiene complicaciones con una Soja de 0.6mt de altura"){}
+    describe("Una parcela que recibe 10hs de sol no tiene problemas con una Soja de 0.8mt de altura"){}
+    describe("Una parcela de ancho: 20m y largo: 1m no puede recibir más de 4 plantas"){}
+
 })
-
-
- */
