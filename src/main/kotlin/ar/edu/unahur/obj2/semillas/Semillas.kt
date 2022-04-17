d34f30f23b9148efff83f18526ce308bf53b589c
Daniela Capponi <82007389+danielaCapponi@users.noreply.github.com>
2022-04-17 11:49:10 -0300

    Format de código

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
@@ -1,53 +1,64 @@
 package ar.edu.unahur.obj2.semillas
 
-abstract class Planta(val anioSemilla : Int, var altura: Double) {
+abstract class Planta(val anioSemilla: Int, var altura: Double) {
     object Constantes {
         val HORAS_TOLERADAS = 9
     }
+
     open fun horasSolQueTolera() = 7
-     fun esFuerte() = horasSolQueTolera() > Constantes.HORAS_TOLERADAS
+    fun esFuerte() = horasSolQueTolera() > Constantes.HORAS_TOLERADAS
     open fun daSemilla() = esFuerte() || condAlternativa()
-    abstract fun condAlternativa() : Boolean
+    abstract fun condAlternativa(): Boolean
 
     abstract fun esIdeal(unaParcela: Parcela): Boolean
 }
 
-open class Menta(anioSemilla: Int, altura: Double) : Planta(anioSemilla, altura){
+open class Menta(anioSemilla: Int, altura: Double) : Planta(anioSemilla, altura) {
     open fun espacio() = altura + 1
     override fun condAlternativa() = altura > 0.4
     override fun esIdeal(unaParcela: Parcela) = unaParcela.superficie() > 6
 }
 
-class Peperina(anioSemilla: Int, altura: Double) : Menta(anioSemilla,altura){
+class Peperina(anioSemilla: Int, altura: Double) : Menta(anioSemilla, altura) {
     override fun espacio() = super.espacio() * 2
 }
 
 
-open class Soja(anioSemilla: Int, altura: Double) : Planta(anioSemilla,altura){
+open class Soja(anioSemilla: Int, altura: Double) : Planta(anioSemilla, altura) {
 
-    fun espacio() = altura/2
+    fun espacio() = altura / 2
     override fun horasSolQueTolera(): Int {
-        return (if (altura < 0.5) {6}
-        else if (altura in 0.5..1.0){8}
-        else {12})
+        return (if (altura < 0.5) {
+            6
+        } else if (altura in 0.5..1.0) {
+            8
+        } else {
+            12
+        })
     }
+
     override fun condAlternativa() = anioSemilla > 2007 && altura in 0.75..0.9
 
     override fun esIdeal(unaParcela: Parcela) = unaParcela.horasSolQueRecibe == horasSolQueTolera()
 
 }
 
-class SojaTrangenica(anioSemilla: Int,altura: Double) : Soja(anioSemilla,altura){
+class SojaTrangenica(anioSemilla: Int, altura: Double) : Soja(anioSemilla, altura) {
     override fun daSemilla() = false
 
-    override fun esIdeal(unaParcela: Parcela) = unaParcela.cantidadMaximaPlantasTolerada() == 1.0
+    override fun esIdeal(unaParcela: Parcela) = unaParcela.cantidadMaximaPlantasTolerada() == 1
 }
 
 
-open class Quinoa(anioSemilla: Int, altura: Double, var espacio: Double): Planta(anioSemilla,altura){
-    override fun horasSolQueTolera()= if(espacio < 0.3) {10} else {super.horasSolQueTolera()}
+open class Quinoa(anioSemilla: Int, altura: Double, var espacio: Double) : Planta(anioSemilla, altura) {
+    override fun horasSolQueTolera() = if (espacio < 0.3) {
+        10
+    } else {
+        super.horasSolQueTolera()
+    }
+
     override fun condAlternativa() = anioSemilla in 2001..2008
-    override fun esIdeal(unaParcela: Parcela) = unaParcela.plantas.all{ it.altura <= 1.5}
+    override fun esIdeal(unaParcela: Parcela) = unaParcela.plantas.all { it.altura <= 1.5 }
 
 }
 
