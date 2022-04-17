b2ff39f71098abc19235011da4e8e2bd5b2d22bf
Daniela Capponi <82007389+danielaCapponi@users.noreply.github.com>
2022-04-17 11:47:44 -0300

    Elimino imports y cambio cuenta que hace promedioPlantasPorParcela

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/INTA.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/INTA.kt
@@ -1,14 +1,11 @@
 package ar.edu.unahur.obj2.semillas
-
-import java.math.BigDecimal
-import java.math.RoundingMode
-import kotlin.math.roundToInt
-import kotlin.math.roundToLong
-
-class INTA(var parcelas : MutableCollection<Parcela>) {
-    fun promedioPlantasPorParcela() : Double {
-        return BigDecimal(parcelas.sumByDouble{ it.cantDePlantasEnParcela() } / parcelas.size).setScale(2, RoundingMode.HALF_EVEN).toDouble()
+class INTA(var parcelas: MutableCollection<Parcela>) {
+    fun promedioPlantasPorParcela(): Int {
+        return parcelas.sumBy { it.cantDePlantasEnParcela() } / parcelas.size
     }
-    fun masSustentable() = parcelasSustentables().find{ c -> c.porcentajeBienAsociada() == parcelasSustentables().maxOf{c.porcentajeBienAsociada()}}
-    fun parcelasSustentables() = parcelas.filter{ it.cantDePlantasEnParcela() >= 4 }
+
+    fun masSustentable() =
+        parcelasSustentables().find { c -> c.porcentajeBienAsociada() == parcelasSustentables().maxOf { c.porcentajeBienAsociada() } }
+
+    fun parcelasSustentables() = parcelas.filter { it.cantDePlantasEnParcela() >= 4 }
 }
